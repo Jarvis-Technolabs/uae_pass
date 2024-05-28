acf1347c1a03a0737bf3b18f72fd59e5790e0092
vipul <vipul.jarvis@gmail.com>
2024-05-28 12:42:48 +0530

    - iOS

--- ios/Classes/UaepassPlugin.swift
+++ ios/Classes/UaepassPlugin.swift
@@ -12,6 +12,21 @@ public class UaepassPlugin: NSObject, FlutterPlugin {
     switch call.method {
     case "getPlatformVersion":
       result("iOS " + UIDevice.current.systemVersion)
+    case "openUaePassApp":
+      if let args = call.arguments as? [String: Any],
+        let appName = args["appName"] as? String,
+        let appPage = args["appPage"] as? String
+      {
+
+        if UIApplication.shared.canOpenURL(URL(string: appName)!) {
+          var appUrl = URL(string: appName + appPage)!
+          UIApplication.shared.open(appUrl, options: [:], completionHandler: nil)
+        } else {
+          result(appName)
+        }
+      } else {
+        result(FlutterError.init(code: "bad args", message: nil, details: nil))
+      }
     default:
       result(FlutterMethodNotImplemented)
     }
